6d2e578288cf63e6f4d5c074c8c2f7ceb3c79afd
Grant <alfred0918@163.com>
2016-02-22 16:10:00 +0800

    add 注解

--- src/main/java/org/nio/example/filechannel/FileChannelExample.java
+++ src/main/java/org/nio/example/filechannel/FileChannelExample.java
@@ -16,15 +16,16 @@ public class FileChannelExample {
         try {
             RandomAccessFile accessFile = new RandomAccessFile("data/nio-data.txt", "rw");
             FileChannel fileChannel = accessFile.getChannel();
+            ////create buffer with capacity of 48 bytes
             ByteBuffer buffer = ByteBuffer.allocate(48);
             int bytesRead = fileChannel.read(buffer); //读取数据到Buffer
             while (bytesRead != -1) {
                 System.out.println("Read " + bytesRead);
-                buffer.flip();//反转Buffer
+                buffer.flip();//将Buffer从写模式切换到读模式
                 while (buffer.hasRemaining()) {
-                    System.out.println((char) buffer.get()); //从Buffer中读取数据
+                    System.out.println((char) buffer.get()); // read 1 byte at a time
                 }
-                buffer.clear();
+                buffer.clear();//make buffer ready for writing
                 bytesRead = fileChannel.read(buffer);
             }
             fileChannel.close();
